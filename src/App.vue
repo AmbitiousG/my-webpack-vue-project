7fdabe2d8abd13fda40600bb3d304cc1e1b99820
Ambitious G <Ambitious G>
2017-03-08 22:53:22 +0800

    set default layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,53 @@
-<template>
+<!-- <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
+    <img src="./assets/logo.png">
     <el-button @click="showProfile">Show Profile</el-button>
     <router-view></router-view>
   </div>
+</template> -->
+<template>
+  <div class="mc-main">
+    <div class="mc-left nav-wrapper">
+      <el-menu default-active="1" class="mc-nav">
+        <el-menu-item index="1">Home</el-menu-item>
+        <el-menu-item index="2">Records</el-menu-item>
+        <el-menu-item index="3">Graph</el-menu-item>
+        <el-menu-item index="4">About</el-menu-item>
+      </el-menu>
+    </div>
+    <div class="mc-right content">
+      <view-router></view-router>
+    </div>
+  </div>
 </template>
+
+<style>
+  html, body{
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  .mc-main{
+    width: 100%;
+    height: 100%;
+    display: flex;
+  }
+  .mc-left, .mc-right{
+    height: 100%;
+  }
+  .mc-left{
+    flex: 0 1 200px;
+  }
+  .mc-right{
+    flex: 1 1 100%;
+  }
+  .mc-nav-wrapper{
+    min-height: 100%;
+  }
+</style>
+
+
 
 <script>
 export default {
@@ -17,13 +60,3 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
