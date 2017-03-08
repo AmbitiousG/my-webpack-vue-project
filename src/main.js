2285193062827ed4667c13313245e173e3247be1
Ambitious G <Ambitious G>
2017-03-08 22:25:52 +0800

    add ui component collection -> ElementUI

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import {Button} from 'element-ui'
+import ElementUI from 'element-ui'
+// import {Button} from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import App from './App'
 import router from './router'
 
-Vue.use(Button)
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
